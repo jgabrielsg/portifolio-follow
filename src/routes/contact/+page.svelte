41408c928320d03d524e4e336c4a516d60d2c827
JOÃO GABRIEL MACHADO <B51065@fgv.edu.br>
2025-04-03 12:30:36 -0300

    first commit

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,26 @@
+
+<svelte:head>
+  <title>Contact me</title>
+</svelte:head>
+<form action="mailto:foo@example.com" method="GET">
+    <label for="email">Email: <input id="email" type="email" name="text_email"> </label>
+    <label for="subject">Subject: <input id="subject" type="text" name="text_subject"> </label>
+    <label for="body">Body: <textarea id="body" name="text_message"></textarea> </label>
+
+    <input type="submit" value="Submit" id="button">
+    <!-- <button>Submit</button> -->
+</form>
+<style>
+  form {
+    display: grid; /*step 3.3 part 1*/
+    grid-template-columns: auto 1fr; /*step 3.3 part 1.1*/
+    gap: 1em; /*step 3.3 part 3*/
+  }
+  form label {
+    display: grid;/*step 3.3 part 2*/
+    grid-template-columns: subgrid;/*step 3.3 part 2*/
+  }
+  #button, label{
+    grid-column: 1 / -1; /*step 3.3 part 2*/
+  }
+</style>
---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,75 @@
+<script>
+  import * as d3 from 'd3';
+  import Pie from '$lib/Pie.svelte';
+  import projects from "$lib/projects.json";
+  import Project from "$lib/Project.svelte";
+
+  let rolledData = d3.rollups(projects, v => v.length, d => d.year);
+
+  let pieData;
+  
+  $: {
+      pieData = rolledData.map(([year, count]) => {
+          return { value: count, label: year };
+      });
+  }
+
+  let query = "";
+
+  // Filtragem por pesquisa 
+  $: filteredProjects = projects.filter(project => {
+      let values = Object.values(project).join("\n").toLowerCase();
+      return values.includes(query.toLowerCase());
+  });
+
+  let selectedYearIndex = -1;
+  let selectedYear;
+  
+  $: selectedYear = selectedYearIndex > -1 ? pieData[selectedYearIndex].label : null;
+
+  // Filtra projetos por ano selecionado
+  $: filteredByYear = filteredProjects.filter(project => {
+      if (selectedYear) {
+          return project.year === selectedYear;
+      }
+      return true;
+  });
+</script>
+
+<svelte:head>
+  <title>Projects</title>
+</svelte:head>
+
+<h1>{ projects.length } Projects</h1>
+
+<Pie data={pieData} bind:selectedIndex={selectedYearIndex} />
+
+<input type="search" bind:value={query} aria-label="Search projects" placeholder="🔍 Search projects..." />
+
+<div class="projects">
+    {#each filteredByYear as p}
+        <Project data={p}/>
+    {/each}
+</div>
+
+<style>
+  h1 {
+      text-align: center;
+  }
+
+  input {
+      display: block;
+      width: 100%;
+      max-width: 400px;
+      margin: 1rem auto;
+      padding: 0.5rem;
+      font-size: 1rem;
+  }
+
+  .projects {
+      display: grid;
+      grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+      gap: 1rem;
+      margin-top: 1rem;
+  }
+</style>
