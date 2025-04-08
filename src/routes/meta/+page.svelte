316ecde0637da45b5e20608ac8a7e9ee3dfaa5de
JOÃO GABRIEL MACHADO <B51065@fgv.edu.br>
2025-04-08 12:28:42 -0300

    correção de link

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -44,7 +44,7 @@
         let {author, date, time, timezone, datetime} = first;
         let ret = {
             id: commit,
-            url: "https://github.com/USERNAME/REPO/commit/" + commit,
+            url: "https://github.com/jgabrielsg/portifolio-follow/commit/" + commit,
             author, date, time, timezone, datetime,
             hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
             totalLines: lines.length
@@ -201,15 +201,9 @@
 
     <dt>Time</dt>
     <dd>{ hoveredCommit.time }</dd>
-
-    <!-- Add: Time, author, lines edited -->
 </dl>
 
 <Bar data={languageBreakdown} width={width} />
-
-
-
-
 
 
 
