af8dbe72463724b5624227d501e36f024f758b4f
JOÃO GABRIEL MACHADO <B51065@fgv.edu.br>
2025-04-24 12:38:25 -0300

    lab7.5

---
+++ src/lib/Scatterplot.svelte
@@ -0,0 +1,133 @@
+<script>
+    import { computePosition, autoPlacement, offset } from '@floating-ui/dom';
+    import { onMount } from "svelte";
+    
+    export let commits = [];
+    export let xScale;
+    export let yScale;
+    export let rScale;
+    export let hoveredIndex = -1;
+    export let hoveredCommit = {};
+    export let clickedCommits = [];
+    export let tooltipPosition = {x: 0, y: 0};
+    export let commitTooltip;
+
+    // Criação e manipulação dos eixos com D3
+    let xAxis, yAxis, yAxisGridlines;
+
+    // Função de interação do ponto
+    async function dotInteraction (index, evt) {
+        let hoveredDot = evt.target;
+        if (evt.type === "mouseenter") {
+            hoveredIndex = index;
+            tooltipPosition = {x: evt.x, y: evt.y};
+            tooltipPosition = await computePosition(hoveredDot, commitTooltip, {
+                strategy: "fixed", // porque usamos position: fixed
+                middleware: [
+                    offset(5), // espaçamento entre o tooltip e o ponto
+                    autoPlacement() // ver https://floating-ui.com/docs/autoplacement
+                ],
+            });
+        }
+        else if (evt.type === "mouseleave") {
+            hoveredIndex = -1
+        }
+        else if (evt.type === "click") {
+            let commit = commits[index]
+            if (!clickedCommits.includes(commit)) {
+                clickedCommits = [...clickedCommits, commit];
+            }
+            else {
+                clickedCommits = clickedCommits.filter(c => c !== commit);
+            }
+        }
+    }
+
+    // Lógica para os eixos
+    onMount(() => {
+        d3.select(xAxis).call(d3.axisBottom(xScale));
+        d3.select(yAxis).call(d3.axisLeft(yScale).tickFormat(d => String(d % 24).padStart(2, "0") + ":00"));
+        d3.select(yAxisGridlines).call(
+            d3.axisLeft(yScale)
+            .tickFormat("")
+            .tickSize(-1000) 
+        );
+    });
+</script>
+
+<svg viewBox="0 0 1000 600">
+    <g transform="translate(0, 600)" bind:this={xAxis} />
+    <g transform="translate(20, 0)" bind:this={yAxis} />
+    <g class="gridlines" transform="translate(20, 0)" bind:this={yAxisGridlines} />
+    <g class="dots">
+        {#each commits as commit, index }
+            <circle
+                on:mouseenter={evt => dotInteraction(index, evt)}
+                on:mouseleave={evt => dotInteraction(index, evt)}
+                on:click={evt => dotInteraction(index, evt)}
+                class:selected={clickedCommits.includes(commit)}
+                cx={xScale(commit.datetime)}
+                cy={yScale(commit.hourFrac)}
+                r={rScale(commit.totalLines)}
+                fill="steelblue"
+                fill-opacity="0.5"
+            />
+        {/each}
+    </g>
+</svg>
+
+<dl class="info tooltip" hidden={hoveredIndex === -1} style="top: {tooltipPosition.y}px; left: {tooltipPosition.x}px" bind:this={commitTooltip}>
+    <dt>Commit</dt>
+    <dd><a href="{ hoveredCommit.url }" target="_blank">{ hoveredCommit.id }</a></dd>
+
+    <dt>Date</dt>
+    <dd>{ hoveredCommit.datetime?.toLocaleString("en", {dateStyle: "full"}) }</dd>
+
+    <dt>Author</dt>
+    <dd>{ hoveredCommit.author }</dd>
+
+    <dt>Time</dt>
+    <dd>{ hoveredCommit.time }</dd>
+</dl>
+
+<style>
+    svg {
+        overflow: visible;
+    }
+
+    .gridlines {
+        stroke-opacity: .2;
+    }
+
+    circle {
+        transition: 200ms;
+        transform-origin: center;
+        transform-box: fill-box;
+        &:hover {
+            transform: scale(1.5);
+        }
+    }
+
+    .selected {
+        fill: var(--color-accent);
+    }
+
+    .info {
+        display: grid;
+        margin: 0;
+        grid-template-columns: 2;
+        background-color: oklch(100% 0% 0 / 80%);
+        box-shadow: 1px 1px 3px 3px gray;
+        border-radius: 5px;
+        backdrop-filter: blur(10px);
+        padding: 10px;
+
+        transition-duration: 500ms;
+        transition-property: opacity, visibility;
+
+        &[hidden]:not(:hover, :focus-within) {
+            opacity: 0;
+            visibility: hidden;
+        }
+    }
+</style>
--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -16,8 +16,19 @@
     $: maxDatePlusOne = new Date(maxDate);
     $: maxDatePlusOne.setDate(maxDatePlusOne.getDate() + 1);
 
+    let commitProgress = 100;
+    $: timeScale = d3.scaleTime().domain([minDate,maxDate]).range([0,100]);
+    $: commitMaxTime = timeScale.invert(commitProgress);
+
+    $: filteredCommits = commits.filter(commit => commit.datetime <= commitMaxTime)
+
+    $: filteredMinDate = d3.min(filteredCommits.map(d => d.date));
+    $: filteredMaxDate = d3.max(filteredCommits.map(d => d.date));
+    $: filteredMaxDatePlusOne = new Date(filteredMaxDate);
+    $: filteredMaxDatePlusOne.setDate(filteredMaxDatePlusOne.getDate() + 1);
+
     $: xScale = d3.scaleTime()
-                .domain([minDate, maxDatePlusOne])
+                .domain([filteredMinDate, filteredMaxDatePlusOne])
                 .range([usableArea.left, usableArea.right])
                 .nice();
 
@@ -44,7 +55,7 @@
         let {author, date, time, timezone, datetime} = first;
         let ret = {
             id: commit,
-            url: "https://github.com/jgabrielsg/portifolio-follow/commit/" + commit,
+            url: "https://github.com/jgabrielsg/PathSeekerAlgorithm/commit/" + commit,
             author, date, time, timezone, datetime,
             hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
             totalLines: lines.length
@@ -79,6 +90,7 @@
 
     let xAxis, yAxis;
     let yAxisGridlines;
+    
 
     $: {
         d3.select(xAxis).call(d3.axisBottom(xScale));
@@ -91,7 +103,7 @@
     }
 
     let hoveredIndex = -1;
-    $: hoveredCommit = commits[hoveredIndex] ?? hoveredCommit ?? {};
+    $: hoveredCommit = filteredCommits[hoveredIndex] ?? hoveredCommit ?? {};
 
     //let cursor = {x: 0, y: 0};
 
@@ -116,7 +128,7 @@
         }
 
         else if (evt.type === "click") {
-            let commit = commits[index]
+            let commit = filteredCommits[index]
             if (!clickedCommits.includes(commit)) {
                 // Add the commit to the clickedCommits array
                 clickedCommits = [...clickedCommits, commit];
@@ -132,7 +144,9 @@
                 .domain(d3.extent(commits.map(d=>d.totalLines)))
                 .range([2, 30]);
     
-    $: allTypes = Array.from(new Set(data.map(d => d.type)));
+    $: filteredLines = data.filter(d => d.datetime <= commitMaxTime)
+    
+    $: allTypes = Array.from(new Set(filteredLines.map(d => d.type)));
     $: selectedLines = (clickedCommits.length > 0 ? clickedCommits : commits).flatMap(d => d.lines);
 
     $: selectedCounts = d3.rollup(
@@ -142,9 +156,6 @@
     );
 
     $: languageBreakdown = allTypes.map(type => [type, selectedCounts.get(type) || 0]);
-
-        
-    
 </script>
 
 <svelte:head>
@@ -160,20 +171,28 @@
     <h2>Summary</h2>
     <dl class="stats">
     <dt>Total <abbr title="Lines of code">LOC</abbr></dt>
-    <dd>{data.length}</dd>
+    <dd>{filteredLines.length}</dd>
     <dt>Files</dt>
-    <dd>{d3.groups(data, d => d.file).length}</dd>
+    <dd>{d3.groups(filteredLines, d => d.file).length}</dd>
     <dt>Commits</dt>
-    <dd>{d3.groups(data, d => d.commit).length}</dd>
+    <dd>{d3.groups(filteredLines, d => d.commit).length}</dd>
     </dl>
 </section>
+
+<div class="slider-container">
+    <div class="slider">
+        <label for="slider">Show commits until:</label>
+        <input type="range" id="slider" name="slider" min=0 max=100 bind:value={commitProgress}/>
+    </div>
+    <time class="time-label">{commitMaxTime.toLocaleString()}</time>
+</div>
 
 <svg viewBox="0 0 {width} {height}">
     <g transform="translate(0, {usableArea.bottom})" bind:this={xAxis} />
     <g transform="translate({usableArea.left}, 0)" bind:this={yAxis} />
     <g class="gridlines" transform="translate({usableArea.left}, 0)" bind:this={yAxisGridlines} />
     <g class="dots">
-        {#each commits as commit, index }
+        {#each filteredCommits as commit, index }
             <circle
                 on:mouseenter={evt => dotInteraction(index, evt)}
                 on:mouseleave={evt => dotInteraction(index, evt)}
@@ -264,6 +283,28 @@
         &:hover {
             transform: scale(1.5);
         }
+        
+        @starting-style {
+	r: 0;
+}
+
+        transition: all 200ms,
+            r calc(var(--r) * 100ms);
     }
+
+    .slider-container{
+	display:grid;
+}
+.slider{
+	display: flex;
+}
+#slider{
+	flex:1;
+}
+.time-label{
+	font-size: 0.75em;
+	text-align: right;
+}
+
 </style>
 
